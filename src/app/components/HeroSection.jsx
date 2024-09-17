0075c96979725b235092bc58479a9e9fdf47b584
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-09-17 21:45:23 +0200

    Thanks God - (p-2 md:p-6) modificação final, para alterar o tamanh do padding para ajustar a navbar

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,60 @@
+"use client";
+import React from 'react'
+import Image from "next/image";
+import { TypeAnimation } from 'react-type-animation';
+
+const HeroSection = () => {
+  return (
+    <section>
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-orange-500 via-red-500 to-black">
+              Hey Gabriel Here!!!!
+            </span><br />
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                'Desenvolvedor',
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                'Front-End',
+                1000,
+                'Back-End',
+                1000,
+                'Full-Stack',
+                1000
+              ]}
+              wrapper="span"
+              speed={50}
+              className="text-base sm:text-2xl"
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl mr-3">
+            Não tenho rede social então desenvolvi esse web-site como um perfil social, para contar minhas histórias e aventuras, seja bem-vinda(o) a elas.
+          </p>
+          <div>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-orange-500 via-red-500 to-black hover:bg-slate-200 text-black">Portifolio</button>
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-orange-500 via-red-500 to-black hover:bg-slate-800 text-white mt-3">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download CV</span>
+            </button>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/profile-pic-removebg-preview.png"
+              alt="hero image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
+          </div>
+
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default HeroSection
---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import HeroSection from "./components/HeroSection.jsx"
+import Navbar from "./components/Navbar.jsx"
+
+export default function Home() {
+  return (
+      <main className="flex min-h-screen flex-col bg-{#121212}">
+        <Navbar />
+        <div class="container mt-24 mx-auto px-12 py-4">
+          <HeroSection />
+        </div>
+      </main>
+  );
+}
