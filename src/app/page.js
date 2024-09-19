7bb8fca3f43510a046930cf8a688439b1095e333
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-09-19 14:28:27 +0200

    Thanks God

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import ProjectSection from "./components/ProjectSection.jsx"
 
 export default function Home() {
   return (
-      <main className="flex min-h-screen flex-col bg-{#121212}">
+      <main className="flex min-h-screen flex-col bg-black">
         <Navbar />
         <div class="container mt-24 mx-auto px-12 py-4">
           <HeroSection />
