bb2fd790a6eac25710b78726dd1c5bd740dd4fc0
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-09-20 20:20:36 +0200

    Thanks God - Corrigindo as Certificações, para somente possuir a verdade e ter os certificados que desejo e aumejo e quero aprender para mimnha vida

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -33,26 +33,15 @@ const TAB_DATA = [
     content: (
       <ul className="list-disc pl-2">
         <li>Algoritmos</li>
-        <li>Big Data & Analytics</li>
         <li>Blockchain</li>
-        <li>Blockchain Advanced</li>
-        <li>Branding Analytics</li>
         <li>Business Management & IT Services</li>
         <li>Customer Experience Management</li>
         <li>Cybersecurity</li>
-        <li>Data Driven Decision</li>
-        <li>Design Thinking</li>
         <li>DevOps & Agile Culture</li>
-        <li>Dominando IoT</li>
-        <li>Future-Proof Organizations</li>
-        <li>Game Design</li>
         <li>Governança de TI</li>
-        <li>Inovação Disruptiva</li>
         <li>Inteligência Artificial e Computacional</li>
         <li>Leadership and Corporate Accountability</li>
         <li>Programação em Banco de Dados</li>
-        <li>Social Media Planning</li>
-        <li>User Experience</li>
       </ul>
     )
   }
@@ -75,7 +64,7 @@ const AboutSection = () => {
       <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
         <h2 className="text-4xl font-bold text-white my-4">Quem Sou</h2>
         <p className="text-base lg:text-lg">
-          Sou musico e desenvolvedor, toco violino e adoro viajar para conhecer novas culturas e línguas. A programação me inspira, pois, por meio dela, posso ajudar empresas a alcançarem seus objetivos e até mesmo realizarem seus sonhos. Tenho mais de 20 formações pela FIAP, a maior faculdade de tecnologia do Brasil. Na foto ao lado, estou tentando &quot;segurar&quot; a Torre de Pisa, pois ela foi &quot;codificada&quot; de forma errada.
+          Sou musico e desenvolvedor, toco violino e adoro viajar para conhecer novas culturas e línguas. A programação me inspira, pois, por meio dela, posso ajudar empresas a alcançarem seus objetivos e até mesmo realizarem seus sonhos. Tenho 10 certificações pela FIAP, a maior faculdade de tecnologia do Brasil. Na foto ao lado, estou tentando &quot;segurar&quot; a Torre de Pisa, pois ela foi &quot;codificada&quot; de forma errada.
         </p>
         <div className="flex flex-row justify-start mt-8">
           <TabButton
