e9f5d33afdc7ada367fa26a2e2d835815ebd60b7
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-09-21 18:33:53 +0200

    Thanks God - Arrumando o tamanho da foto

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -6,7 +6,7 @@ import { TypeAnimation } from 'react-type-animation';
 const HeroSection = () => {
   return (
     <section>
-      <div className="grid grid-cols-1 sm:grid-cols-12">
+      <div className="grid grid-cols-1 md:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-orange-500 via-red-500 to-[#202020]">
@@ -34,8 +34,8 @@ const HeroSection = () => {
           Como não uso redes sociais, criei este site para compartilhar minhas certificações, momentos das minhas viagens e os lugares por onde passei. Seja bem-vindo(a) a minha vida!
           </p>
           <div>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-orange-500 via-red-500 to-black hover:bg-slate-200 text-black">Trabalhos</button>
-            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-orange-500 via-red-500 to-black hover:bg-slate-800 text-white mt-3">
+            <button className="px-6 py-3 w-full md:w-fit rounded-full mr-4 bg-gradient-to-br from-orange-500 via-red-500 to-black hover:bg-slate-200 text-black">Trabalhos</button>
+            <button className="px-1 py-1 w-full md:w-fit rounded-full bg-gradient-to-br from-orange-500 via-red-500 to-black hover:bg-slate-800 text-white mt-3">
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download CV</span>
             </button>
           </div>
